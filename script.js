d0ea6c663bc212ff9debb21421f786ea54bda8be
salmvenera <ahmetovavm@gmail.com>
2021-10-05 13:01:02 +0500

    усложненное 3 ур

---
+++ script.js
@@ -0,0 +1,29 @@
+/*var num = 266219;
+var cool = 1
+for(var i = 0; i <= num.lenght-1; i++) {
+    cool = cool * Number(number[i]) {
+        alert(i);
+    }
+}
+*/
+
+let lang = 'Дни недели';
+lang = prompt('Ru or en?');
+console.log(lang);
+if (lang = 'ru') {
+    console.log('Понедельник');
+    console.log('Вторник');
+    console.log('Среда');
+    console.log('Четверг');
+    console.log('Пятница');
+    console.log('Суббота');
+    console.log('Воскресенье');
+} else (lang = 'en') {
+    console.log('Monday');
+    console.log('Tuesday');
+    console.log('Wednesday');
+    console.log('Thursday');
+    console.log('Friday');
+    console.log('Saturday');
+    console.log('Sunday');
+}
